1df6d67fd0c2690e441d48417acc00e63025636d
LucaHölzel <blankflankpictures@gmail.com>
2019-01-21 16:21:59 +0100

    UI System

---
+++ P3_Sonnenschein/Assets/0_Scripts/UI/UI_ButtonIndicator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ButtonIndicator : MonoBehaviour, UI_Obj_Interface {
+
+    public void setAnchorObject(GameObject obj, Vector3 offset)
+    {
+        transform.parent = obj.transform;
+        transform.localPosition = offset;
+    }
+
+	public void setImg(Texture2D imgTex)
+    {
+        Material buttonMat = new Material(Shader.Find("Unlit/Transparent Cutout"));
+        buttonMat.SetTexture("_MainTex", imgTex);
+        transform.GetChild(0).GetComponent<Renderer>().material = buttonMat;
+    }
+
+    public void setText(string nText)
+    {
+        transform.GetChild(1).GetComponent<TextMesh>().text = nText;
+        //set size based on length
+    }
+
+    public void LateUpdate()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(90, -90, 90));
+        transform.localScale = new Vector3(0.3330514f, 0.3330514f, 0.1132375f);
+    }
+}
