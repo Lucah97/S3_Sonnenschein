1b837e21a9df22839816d437e7aec1b9d2667f0c
Christoph Warzecha <warzecha.christoph@gmail.com>
2019-01-22 14:44:41 +0100

    pls let this fix it

--- P3_Sonnenschein/Assets/0_Scripts/Interfaces/UI_Obj_Interface.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public interface UI_Obj_Interface {
-
-    void setAnchorObject(GameObject obj, Vector3 offset);
-    void setImg(Texture2D imgTex);
-    void setText(string nText);
-
-}
